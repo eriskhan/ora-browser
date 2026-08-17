87306c63f4ffb635a4458525f9ac0a7e26b55c3c
Eriskhan Geshaev <erik@geshaev.eu>
2026-08-17 11:25:41 +0100

    feat: persist original extension permissions

--- ora/Features/Extensions/Models/InstalledWebExtension.swift
+++ ora/Features/Extensions/Models/InstalledWebExtension.swift
@@ -10,8 +10,10 @@ struct InstalledWebExtension: Codable, Hashable, Identifiable {
     var installedAt: Date
     var isEnabled: Bool
     var permissionDecisionMade: Bool
+    var originalPermissions: Set<String>
     var grantedPermissions: Set<String>
     var grantedMatchPatterns: Set<String>
+    var compatibilityRevision: Int
 
     init(
         id: UUID,
@@ -23,8 +25,10 @@ struct InstalledWebExtension: Codable, Hashable, Identifiable {
         installedAt: Date = Date(),
         isEnabled: Bool = true,
         permissionDecisionMade: Bool = false,
+        originalPermissions: Set<String> = [],
         grantedPermissions: Set<String> = [],
-        grantedMatchPatterns: Set<String> = []
+        grantedMatchPatterns: Set<String> = [],
+        compatibilityRevision: Int = 0
     ) {
         self.id = id
         self.runtimeIdentifier = runtimeIdentifier
@@ -35,7 +39,42 @@ struct InstalledWebExtension: Codable, Hashable, Identifiable {
         self.installedAt = installedAt
         self.isEnabled = isEnabled
         self.permissionDecisionMade = permissionDecisionMade
+        self.originalPermissions = originalPermissions
         self.grantedPermissions = grantedPermissions
         self.grantedMatchPatterns = grantedMatchPatterns
+        self.compatibilityRevision = compatibilityRevision
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case runtimeIdentifier
+        case name
+        case version
+        case manifestVersion
+        case resourceRelativePath
+        case installedAt
+        case isEnabled
+        case permissionDecisionMade
+        case originalPermissions
+        case grantedPermissions
+        case grantedMatchPatterns
+        case compatibilityRevision
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        runtimeIdentifier = try container.decode(String.self, forKey: .runtimeIdentifier)
+        name = try container.decode(String.self, forKey: .name)
+        version = try container.decode(String.self, forKey: .version)
+        manifestVersion = try container.decode(Double.self, forKey: .manifestVersion)
+        resourceRelativePath = try container.decode(String.self, forKey: .resourceRelativePath)
+        installedAt = try container.decodeIfPresent(Date.self, forKey: .installedAt) ?? Date.distantPast
+        isEnabled = try container.decodeIfPresent(Bool.self, forKey: .isEnabled) ?? true
+        permissionDecisionMade = try container.decodeIfPresent(Bool.self, forKey: .permissionDecisionMade) ?? false
+        originalPermissions = try container.decodeIfPresent(Set<String>.self, forKey: .originalPermissions) ?? []
+        grantedPermissions = try container.decodeIfPresent(Set<String>.self, forKey: .grantedPermissions) ?? []
+        grantedMatchPatterns = try container.decodeIfPresent(Set<String>.self, forKey: .grantedMatchPatterns) ?? []
+        compatibilityRevision = try container.decodeIfPresent(Int.self, forKey: .compatibilityRevision) ?? 0
     }
 }
