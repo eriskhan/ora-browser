041675f5f8dd646a66d6ebd57b71fe8b89f0e704
Eriskhan Geshaev <erik@geshaev.eu>
2026-08-17 12:44:32 +0100

    feat: bump Mozilla compatibility revision

--- ora/Features/Extensions/Compatibility/WebExtensionPackagePreparer.swift
+++ ora/Features/Extensions/Compatibility/WebExtensionPackagePreparer.swift
@@ -26,7 +26,7 @@ enum WebExtensionPackagePreparer {
         }
     }
 
-    static let currentCompatibilityRevision = 3
+    static let currentCompatibilityRevision = 4
     static let internalBridgePermission = "nativeMessaging"
     private static let bridgeWorkerFileName = "__ora_mozilla_background.js"
     private static let bridgeWorkerTargetFileName = "__ora_mozilla_background_target.txt"
